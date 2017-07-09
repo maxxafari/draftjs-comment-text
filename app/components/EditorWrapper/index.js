0d60b36f1e8d5eb6f84a6cf35d7f03bb83d13d64
Max Berkenstam (Exp) <max.berkenstam@expressen.se>
2017-07-09 12:16:46 +0200

    add key commands to editor

--- app/components/EditorWrapper/index.js
+++ app/components/EditorWrapper/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { Editor } from 'draft-js';
+import { Editor, RichUtils } from 'draft-js';
 require('draft-js/dist/Draft.css');
-
 
 const style = { background: 'white', border: '1px solid black', minHeight: '200px' };
 
 class EditorWrapper extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  handleKeyCommand(command) {
+    console.log('handle..');
+    const { editorState, setEditorState } = this.props;
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      console.log('handeld', command);
+      return 'handled';
+    }
+    console.log('not handeld,', command);
+    return 'not-handled';
+  }
 
   render() {
     const { editorState, setEditorState } = this.props;
     return (
       <div style={style}>
         <p>Add notes by selecting text</p>
-        <Editor editorState={editorState} onChange={setEditorState} />
+        <Editor handleKeyCommand={this.handleKeyCommand} editorState={editorState} onChange={setEditorState} />
       </div>
     );
   }
